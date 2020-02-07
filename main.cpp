fc4837edd71a939ec7b3c81c1588e2ff05d8835f
Nate Lenze <nlenze@umich.edu>
2020-02-06 22:14:46 -0600

    add myName function

--- main.cpp
+++ main.cpp
@@ -3,11 +3,20 @@
 
 using namespace std;
 
+
+void printMyName(string myName){
+	cout<<"my name is: "<<myName<<endl;
+}
+
+
+
 int main( int argc, const char* argv[] )
 {
   cout << "sup bro" << endl;
   cout << "not much, bro" << endl;
   cout<<"hello"<<endl;
+
+  printMyName("Nate");
 
 	return 0;
 }
