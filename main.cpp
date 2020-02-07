1068d4662802f6114820807530e6de994c942b5e
jvsnotag <jvsnotag@gmail.com>
2020-02-07 00:15:36 -0500

    distTwo

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
@@ -19,12 +20,12 @@ struct coordinates{
 	string houseName;
 };
 
-struct h1;
-struct h2;
+struct coordinates h1;
+struct coordinates h2;
 
-void distTwo(struct house1, struct house2){
+void distTwo(struct coordinates house1, struct coordinates house2){
 	float distance = sqrt(pow(house2.x-house1.x,2)+pow(house2.y-house1.y,2)*1.0);
-	cout<<"The distance between "<<h1.houseName<<" and "<<h2.houseName<<" is "<<distance<<".";
+	cout<<"The distance between "<<house1.houseName<<" and "<<house2.houseName<<" is "<<distance<<".";
 }
 
 int main( int argc, const char* argv[] )
@@ -38,11 +39,11 @@ int main( int argc, const char* argv[] )
 
   h1.x=10;
   h1.y=20;
-  h1.houseName=yeetYah;
+  h1.houseName="yeetYah";
 
   h2.x=35;
   h2.y=45;
-  h2.houseName=yeetNah;
+  h2.houseName="yeetNah";
 
   distTwo(h1,h2);
   
