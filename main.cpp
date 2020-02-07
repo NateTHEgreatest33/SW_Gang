11f68b0f2fca54c036332b99fe26412465caae49
jvsnotag <jvsnotag@gmail.com>
2020-02-06 23:56:27 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,6 +13,19 @@ void addTwo(int inputNum){
 	cout<<"The resulting number is: "<< inputNum << endl;
 }
 
+struct coordinates{
+	float x;
+	float y;
+	string houseName;
+};
+
+struct h1;
+struct h2;
+
+void distTwo(struct house1, struct house2){
+	float distance = sqrt(pow(house2.x-house1.x,2)+pow(house2.y-house1.y,2)*1.0);
+	cout<<"The distance between "<<h1.houseName<<" and "<<h2.houseName<<" is "<<distance<<".";
+}
 
 int main( int argc, const char* argv[] )
 {
@@ -22,6 +35,17 @@ int main( int argc, const char* argv[] )
 
   printMyName("Nate");
   addTwo(4);
+
+  h1.x=10;
+  h1.y=20;
+  h1.houseName=yeetYah;
+
+  h2.x=35;
+  h2.y=45;
+  h2.houseName=yeetNah;
+
+  distTwo(h1,h2);
+  
 
 	return 0;
 }
