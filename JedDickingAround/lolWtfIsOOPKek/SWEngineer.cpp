93960834cc8d6f88e7f7634da4b84655593aa6a1
jmacatul <jmacatul@umich.edu>
2020-02-07 17:02:16 -0500

    Trying out GitHub Desktop

---
+++ JedDickingAround/lolWtfIsOOPKek/SWEngineer.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+#include "SWEngineer.h"
+
+using namespace std;
+
+void SWEngineer::doJob() {
+    cout << "Beep Boop Beep Boop" << endl;
+}
---
+++ JedDickingAround/lolWtfIsOOPKek/SWEngineer.h
@@ -0,0 +1,13 @@
+#ifndef SWENGINEER_H
+#define SWENGINEER_H
+#include "Person.h"
+
+using namespace std;
+
+class SWEngineer : public Person
+{
+    public:
+        void SWEngineer::doJob();
+};
+
+#endif
